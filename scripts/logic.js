91055eb997d72014322baff7b996cd4c1ea0f7b9
h3d0 <obiolg@gmail.com>
2021-03-13 15:11:55 +0200

    Finish menu for handset

--- scripts/logic.js
+++ scripts/logic.js
@@ -8,12 +8,43 @@ function docReady(fn) {
 
 docReady(function () {
   // DOM variables
+  var contentBurger = document.getElementById('content__burger');
+  var contentItems = document.getElementById('content__items');
+  var contentItem = contentItems.querySelectorAll('.items__item');
+  var fade = document.getElementById('fade');
   var faqQuestions = document.querySelectorAll('.faq__question');
 
   // Listeners
+  contentBurger.addEventListener('click', onContentBurgerClick);
+  contentItem.forEach(function (m) {
+    m.addEventListener('click', onContentItemClick);
+  });
+  fade.addEventListener('click', onFadeClick);
   faqQuestions.forEach(function (m) {
     m.addEventListener('click', onFaqQuestionClick);
   });
+
+  /**
+   * Toggle menu state in handset mode.
+   */
+  function onContentBurgerClick() {
+    contentItems.classList.toggle('content__items--expanded');
+    fade.classList.toggle('fade--visible');
+  }
+
+  function onFadeClick() {
+    contentItems.classList.remove('content__items--expanded');
+    fade.classList.remove('fade--visible');
+  }
+
+  /**
+   *  Hide fade and menu before navigating.
+   * @param {MouseEvent} e
+   */
+  function onContentItemClick(e) {
+    contentItems.classList.remove('content__items--expanded');
+    fade.classList.remove('fade--visible');
+  }
 
   /**
    *  Toggle collapsed/expended state.
